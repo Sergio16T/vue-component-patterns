b7a7765cda6a8bd4baf024165aa5d21789d2c9ed
Sergio Tapia-Fikes <stapiafikes@gmail.com>
2022-06-26 20:18:34 -0500

    First Commit: Vue 3 setup

---
+++ component-patterns/src/components/Tabs/TabProvider.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    data: () => ({
+        tabIndex: 0,
+    }),
+
+    provide() {
+        return {
+            tabIndex: this.tabIndex,
+            setTabIndex: this.setTabIndex,
+        };
+    },
+
+    methods: {
+        setTabIndex(index) {
+            this.tabIndex = index;
+        },
+    }
+}
+</script>
+
+<template lang="">
+    <div>
+        <slot></slot>
+    </div>
+</template>
+<style lang="">
+
+</style>
---
+++ component-patterns/src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TabProvider from "../components/Tabs/TabProvider.vue";
+</script>
+
+<template>
+  <div class="about">
+    <TabProvider/>
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
